cb44645d4c34e3a22886a145a6fc206bb9da2711
josh <>
2004-02-20 20:09:22 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -245,13 +245,13 @@ function genpreview($dir,$reldir,$add,$link){
 		$source = "$dir/".$pics[0];
 	}
 	if($source != ""){
+		$size = @getimagesize($source);
+		if($size && $size[1] > $size[0]){ $flip=1; }
 		$flip=0;
 		if(!file_exists("$dir/pv_thumb.jpg") or (filectime($source) > filectime("$dir/pv_thumb.jpg"))){
 			# here is where to check
 			$cmd = "$convert -resize $pv_thumbWidth"."x"."$pv_thumbHeight \"$source\" \"$dir/pv_thumb.jpg\"";
-			$size = @getimagesize($source);
-			if($size && $size[1] > $size[0]){
-				$flip=1;
+			if($flip){
 				$cmd = "$convert -resize $pv_thumbHeight"."x"."$pv_thumbWidth \"$source\" \"$dir/pv_thumb.jpg\"";
 			}
 			`$cmd`;
